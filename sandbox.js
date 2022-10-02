21ec9ced96d49ffb4f68a1b868e224a9f6438bf9
Jonasson Oscar <oscjjonasson@gmail.com>
2022-10-02 17:05:21 +0300

    end of lesson

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,31 @@ var age;
 var isLoggedIn;
 // age = 'luigi';
 age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
 // arrays
+// with empty array, able to push
+var ninjas = [];
+// ninjans = [10, 24];
+// ninjas = ['yoshi', 'mario'];
+ninjas.push('zeku');
 // union types
+// in front of array, types need to be inside parenthesis
+var mixed = [];
+mixed.push(32, 'zangief');
+// type needs to be added to mixed array
+mixed.push(true);
+var uid;
+uid = '123';
+uid = 123;
+// uid = false;
 // objects
+var ninjaOne;
+ninjaOne = { name: 'zeku', age: 45 };
+ninjaOne = 'hello';
+var ninjaTwo;
+ninjaTwo = {
+    name: 'mario',
+    age: 20,
+    beltColor: 'white'
+};
--- sandbox.ts
+++ sandbox.ts
@@ -5,8 +5,48 @@ let isLoggedIn: boolean;
 
 // age = 'luigi';
 age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
 // arrays
 
+// with empty array, able to push
+let ninjas: string[] = [];
+
+// ninjans = [10, 24];
+// ninjas = ['yoshi', 'mario'];
+
+ninjas.push('zeku');
+
 // union types
+// in front of array, types need to be inside parenthesis
+let mixed: (string | number | boolean)[] = [];
+mixed.push(32, 'zangief');
+// type needs to be added to mixed array
+mixed.push(true);
+
+let uid: string | number;
+
+uid = '123';
+uid = 123;
+// uid = false;
 
 // objects
+
+let ninjaOne: object;
+ninjaOne = { name: 'zeku', age: 45 };
+ninjaOne = 'hello';
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColor: string;
+};
+
+ninjaTwo = {
+  name: 'mario',
+  age: 20,
+  beltColor: 'white',
+  //   skills: ['kick', 'cape'],
+};
