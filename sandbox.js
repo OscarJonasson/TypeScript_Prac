21fc6a921feffa0bba265579aedf99c62776cf12
Jonasson Oscar <oscjjonasson@gmail.com>
2022-10-02 16:07:07 +0300

    lesson05

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,4 @@
+// explicit types
 // arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('mikko');
-// numbers[3] = 'mikko';
-var mixed = ['ken', 4, 'blanka', 20];
-mixed.push('vega');
-mixed.push(30);
-mixed[0] = 3;
+// union types
 // objects
-var ninja = {
-    name: 'mario',
-    belt: 'glitter',
-    age: 44
-};
-ninja.age = 40;
-ninja.name = 'bison';
-// needs to be a number
-// ninja.age = '23';
-// need to be declared in ninja before adding
-// ninja.skills = ['judo', 'kick'];
-ninja = 'asdf';
-// all of these need to match the original object to be changed, without age is not allowed/ with is ok
-ninja = {
-    name: 'yoshi',
-    belt: 'pink',
-    age: 42
-};
--- sandbox.ts
+++ sandbox.ts
@@ -1,44 +1,7 @@
+// explicit types
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
 
-let numbers = [10, 20, 30, 40];
-
-numbers.push(25);
-// numbers.push('mikko');
-// numbers[3] = 'mikko';
-
-let mixed = ['ken', 4, 'blanka', 20];
-
-mixed.push('vega');
-mixed.push(30);
-mixed[0] = 3;
+// union types
 
 // objects
-
-let ninja = {
-  name: 'mario',
-  belt: 'glitter',
-  age: 44,
-};
-
-ninja.age = 40;
-ninja.name = 'bison';
-
-// needs to be a number
-// ninja.age = '23';
-
-// need to be declared in ninja before adding
-// ninja.skills = ['judo', 'kick'];
-
-ninja = 'asdf';
-
-// all of these need to match the original object to be changed, without age is not allowed/ with is ok
-ninja = {
-  name: 'yoshi',
-  belt: 'pink',
-  age: 42,
-  //   skills: ['kick'],
-};
