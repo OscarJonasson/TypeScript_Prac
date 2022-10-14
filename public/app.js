8f5950282a4e4c4b9cf4b965c4446e86e4472f41
Jonasson Oscar <oscjjonasson@gmail.com>
2022-10-14 14:01:42 +0300

    lesson14

--- public/app.js
+++ public/app.js
@@ -1,24 +1,11 @@
-"use strict";
-// Classes
-class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/invoice';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// console.log(invOne, invTwo);
 let invoices = [];
-// invoices.push('hello');
 invoices.push(invOne);
 invoices.push(invTwo);
 invoices.forEach((inv) => {
-    console.log(inv.client, inv.details, inv.amount, 'Whole invoice:', inv.format());
+    console.log(inv.client, inv.amount, 'Whole invoice:', inv.format());
 });
 const form = document.querySelector('.new-item-form');
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,15 @@
-// Classes
-class Invoice {
-  //   readonly client: string;
-  //   // if private can only be accessed inside the class
-  //   private details: string;
-  //   public amount: number;
-
-  //   constructor(c: string, d: string, a: number) {
-  //     this.client = c;
-  //     this.details = d;
-  //     this.amount = a;
-  //   }
-
-  // Shorthand, you need to use the access modifiers for this to work
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    // this.client = `something else`;
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/invoice';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-// console.log(invOne, invTwo);
-
 let invoices: Invoice[] = [];
-// invoices.push('hello');
+
 invoices.push(invOne);
 invoices.push(invTwo);
 
 invoices.forEach((inv) => {
-  // readonly inside the class, can still read it (console log)
-  //   inv.client = 'something else';
   console.log(inv.client, inv.amount, 'Whole invoice:', inv.format());
-  //without private
-  //   console.log(
-  //     inv.client,
-  //     inv.details,
-  //     inv.amount,
-  //     'Whole invoice:',
-  //     inv.format()
-  //   );
 });
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
