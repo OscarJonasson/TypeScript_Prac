7b33ff1d2a1b084aeca0f968759096f06446a1b0
Jonasson Oscar <oscjjonasson@gmail.com>
2022-10-14 13:08:32 +0300

    lesson11

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 "use strict";
+// ! if you are certain that it will exist
+// const anchor = document.querySelector('a')!;
+// if (anchor) {
+//   // could possibly be null so, possible fix is if
+//   console.log(anchor.href);
+// }
+// console.log(anchor.href);
+// form grab
+// const form = document.querySelector('form')!;
+// const form = document.querySelector('.new-item-form')!;
+// typeCasting
+const form = document.querySelector('.new-item-form');
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// ! if you are certain that it will exist
+// const anchor = document.querySelector('a')!;
+// if (anchor) {
+//   // could possibly be null so, possible fix is if
+//   console.log(anchor.href);
+// }
+
+// console.log(anchor.href);
+
+// form grab
+
+// const form = document.querySelector('form')!;
+
+// const form = document.querySelector('.new-item-form')!;
+
+// typeCasting
+const form = document.querySelector('.new-item-form')!;
