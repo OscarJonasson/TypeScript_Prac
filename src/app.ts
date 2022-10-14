f4a7d13026fa7142b1ab84bd07797d770f42ea34
Jonasson Oscar <oscjjonasson@gmail.com>
2022-10-14 13:25:19 +0300

    lesson11 end

--- src/app.ts
+++ src/app.ts
@@ -14,4 +14,17 @@
 // const form = document.querySelector('.new-item-form')!;
 
 // typeCasting
-const form = document.querySelector('.new-item-form')!;
+// const form = document.querySelector('.new-item-form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
